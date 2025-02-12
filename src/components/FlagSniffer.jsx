631350f2ffc3b2200ad0a8668e6016af7f16612b
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-02-12 15:55:48 +0100

    feat: add popular flags

--- src/components/FlagSniffer.jsx
+++ src/components/FlagSniffer.jsx
@@ -1,8 +1,12 @@
 export default function FlagSniffer(elm) {
   let url = "";
-  console.log(elm);
+
   if (elm.original_language === "en") {
     url = `https://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg`;
+  } else if (elm.original_language === "ja") {
+    url = `https://purecatamphetamine.github.io/country-flag-icons/3x2/JP.svg`;
+  } else if (elm.original_language === "ko") {
+    url = `https://purecatamphetamine.github.io/country-flag-icons/3x2/KR.svg`;
   } else {
     url = `https://purecatamphetamine.github.io/country-flag-icons/3x2/${elm.original_language.toUpperCase()}.svg`;
   }
